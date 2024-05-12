ac7a0c76575f0abefc3e211ee7848b6503575464
KristiinaTam <kristiinatam@gmail.com>
2024-05-12 13:23:25 -0600

    chachanges

--- index.js
+++ index.js
@@ -1,6 +1,40 @@
-var navButtons = document.querySelectorAll('.nav-btn');
-navButtons.forEach(function(button) {
-  button.addEventListener('click', function() {
-    this.classList.toggle('collapse');
-  });
-});
+
+      var greenChicken = document.getElementById("greenChicken");
+      if (greenChicken) {
+        greenChicken.addEventListener("click", function () {
+          var anchor = document.querySelector("[data-scroll-to='headingText3']");
+          if (anchor) {
+            anchor.scrollIntoView({ block: "start" });
+          }
+        });
+      }
+      
+      var pinkChicken = document.getElementById("pinkChicken");
+      if (pinkChicken) {
+        pinkChicken.addEventListener("click", function () {
+          var anchor = document.querySelector("[data-scroll-to='text11']");
+          if (anchor) {
+            anchor.scrollIntoView({ block: "start" });
+          }
+        });
+      }
+      
+      var yellowChicken = document.getElementById("yellowChicken");
+      if (yellowChicken) {
+        yellowChicken.addEventListener("click", function () {
+          var anchor = document.querySelector("[data-scroll-to='placeholderImage1']");
+          if (anchor) {
+            anchor.scrollIntoView({ block: "start" });
+          }
+        });
+      }
+      
+      var blueChicken = document.getElementById("blueChicken");
+      if (blueChicken) {
+        blueChicken.addEventListener("click", function () {
+          var anchor = document.querySelector("[data-scroll-to='placeholderImage']");
+          if (anchor) {
+            anchor.scrollIntoView({ block: "start" });
+          }
+        });
+      }
